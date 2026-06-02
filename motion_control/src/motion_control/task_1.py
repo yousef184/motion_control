3371d2daab603da6498cee3e0b325b04121412aa
ming <ming@IMR.com>
2026-06-02 15:39:35 +0200

    Task1: parse released order nodes and build route

--- motion_control/src/motion_control/task_1.py
+++ motion_control/src/motion_control/task_1.py
@@ -3,6 +3,7 @@ This script is an example code to be used for individual task 1.
 '''
 
 import json
+from platform import node
 import paho.mqtt.client as mqtt
 
 class Robot:
@@ -16,17 +17,41 @@ class Robot:
         Process the received order message.
 
         Students should:
-        - Decode the JSON payload
-        - Extract 'nodes' and 'edges' from the message
-        - Filter only the 'released' ones
-        - Store relevant information in self.nodes and self.edges
+        - Decode the JSON payload  ---1
+        - Extract 'nodes' and 'edges' from the message ---2
+        - Filter only the 'released' ones ---3 
+        - Store relevant information in self.nodes and self.edges ---4
 
         Example (to be implemented):
         # for node in nodes:
         #     print(f"Node ID: {node['nodeId']} at ({node['x']}, {node['y']})")
         """
         # TODO: Implement order parsing and printing logic here
-        pass
+        payload = json.loads(msg.payload.decode("utf-8")) #1
+
+        nodes = payload.get("nodes", []) #2
+        edges = payload.get("edges", []) #2
+
+        self.nodes = [n for n in nodes if n.get("released", False)] #3
+        self.edges = [e for e in edges if e.get("released", False)] #3
+
+    route = []  #4  并且打印出每个节点的ID和坐标信息
+    for node in self.nodes:
+        node_id = node.get("nodeId")
+        pos = node.get("nodePosition", {})
+        x = pos.get("x")
+        y = pos.get("y")
+
+    if x is not None and y is not None:
+        route.append((x, y))
+        print(f"nodeId={node_id}, x={x}, y={y}")
+    else:
+        print(f"nodeId={node_id}, nodePosition missing x/y: {pos}")
+
+    print("Released route:", route)
+
+
+    pass
 
 def on_connect(client, userdata, flags, rc):
     client.subscribe(userdata["topic_order"])
